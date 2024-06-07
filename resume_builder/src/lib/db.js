b37a0f245ff059827a4489115c5424c01f93eb21
anshkakkar15 <kakkaransh40@gmail.com>
2024-06-08 01:41:31 +0530

    first commit

---
+++ resume_builder/src/lib/db.js
@@ -0,0 +1,27 @@
+import { mongoose } from "mongoose";
+
+const mongoDbUri = process.env.MONGO_URL;
+
+if (!mongoDbUri) {
+  throw new Error("Please define the DATABASE_URL in environment variable");
+}
+
+const connection = {};
+
+async function dbConnect() {
+  if (connection.isConnected) {
+    console.log("Database is already connected");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(mongoDbUri);
+    connection.isConnected = db.connections[0].readyState;
+    console.log("database connection successfully");
+  } catch (error) {
+    console.log("Errow while connecting to database", error);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
