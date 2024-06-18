5587e3285b8eaf4cb35d28b0450e38a997978307
anshkakkar15 <kakkaransh40@gmail.com>
2024-06-19 01:16:04 +0530

    error removed

--- cvmaster/src/components/Footer.jsx
+++ cvmaster/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { ASSETS } from "../../assets";
 
 export const Footer = () => {
   return (
-    <div className="my-5 border-b-[1px] border-t-[1px] border-dark-blue bg-white">
+    <div className="mt-5 border-b-[1px] border-t-[1px] border-dark-blue bg-white">
       <div className="container mx-auto px-3 sm:px-5 md:px-7 lg:px-8">
         <div className="flex flex-wrap items-center justify-between gap-y-3 py-3 sm:py-4">
           <div className="max-w-28 sm:max-w-32 2xl:w-full">
