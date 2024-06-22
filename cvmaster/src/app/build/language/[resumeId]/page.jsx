ee14d2523d4e2803e16904c5282dce9df2350700
anshkakkar15 <kakkaransh40@gmail.com>
2024-06-23 01:37:02 +0530

    changes done

--- cvmaster/src/app/build/language/[resumeId]/page.jsx
+++ cvmaster/src/app/build/language/[resumeId]/page.jsx
@@ -9,56 +9,88 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { backStep } from "@/lib/getBuilderPage";
-import { Plus } from "lucide-react";
+import { languageAndSkillSchema } from "@/schemas/languageAndSkillSchema";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Plus, X } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useImperativeHandle, useRef } from "react";
+import { useForm, useFieldArray } from "react-hook-form";
 
 export default function Language() {
   const { resumeId } = useParams();
   const router = useRouter();
+  const formRef = useRef();
 
-  const [input, setInput] = useState([{ input: "" }]);
+  const form = useForm({
+    defaultValues: {
+      languages: [{ language: "" }],
+    },
+    resolver: yupResolver(languageAndSkillSchema),
+  });
 
-  const form = useForm();
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "languages",
+  });
+
+  useImperativeHandle(formRef, () => ({
+    submit: form.handleSubmit(handleAddLanguage),
+  }));
+
+  const handleContinue = () => {
+    if (formRef.current) {
+      formRef.current.submit();
+    }
+  };
+
+  const handleAddLanguage = (data) => {
+    console.log(data);
+    // router.push(`/build/summary/${resumeId}`);
+    // nextStep("summary");
+  };
 
   return (
     <>
       <BuilderLayout
-        heading="Let's Add your prefered lanuages"
+        heading="Let's add your preferred languages"
         description="Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Doloremque dolorem dignissimos rerum"
         handleBack={() => {
           router?.push(`/build/summary/${resumeId}`);
           backStep("language");
         }}
+        handleContinue={handleContinue}
       >
         <Form {...form}>
           <form>
             <FormLabel>LANGUAGE</FormLabel>
-            {input?.length >= 1 &&
-              input.map((_, i) => {
-                return (
-                  <FormField
-                    key={i}
-                    control={form.control}
-                    name={`language_${i + 1}`}
-                    render={({ field }) => (
-                      <FormItem>
-                        <Input
-                          {...field}
-                          name={`language_${i + 1}`}
-                          placeholder="LANGUAGE"
-                          className="mt-3"
-                        />
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                );
-              })}
+            {fields.map((field, index) => (
+              <FormField
+                key={field.id}
+                control={form.control}
+                name={`languages.${index}.language`}
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex items-center gap-4">
+                      <Input
+                        {...field}
+                        placeholder="LANGUAGE"
+                        className="mt-3"
+                      />
+                      <div
+                        className="mt-3 cursor-pointer rounded-md bg-dark-blue p-2 text-white"
+                        onClick={() => remove(index)}
+                      >
+                        <X />
+                      </div>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ))}
             <div
-              onClick={() => setInput([...input, { input: "" }])}
+              onClick={() => append({ language: "" })}
               className="mt-5 flex cursor-pointer items-center gap-2"
             >
               <Plus /> Add More
